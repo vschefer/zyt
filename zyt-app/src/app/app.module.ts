c3ee821f4eef588a800d8b289602109e6810b4af
vschefer <vanessa.schefer@hotmail.com>
2018-10-11 13:52:31 +0200

    add fundamental components

--- zyt-app/src/app/app.module.ts
+++ zyt-app/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { WorkingTimeComponent } from './working-time/working-time.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
+import { TimeComponent } from './dashboard/time/time.component';
+import { UserProfilComponent } from './dashboard/user-profil/user-profil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavigationComponent } from './dashboard/navigation/navigation.component';
+import { CommonModule } from '@angular/common';
+import { PlanComponent } from './plan/plan.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkingTimeComponent,
+    TodoListComponent,
+    TodoDetailComponent,
+    UserProfilComponent,
+    TimeComponent,
+    DashboardComponent,
+    NavigationComponent,
+    PlanComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ zyt-app/src/app/working-time/working-time.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkingTimeComponent } from './working-time.component';
+
+describe('WorkingTimeComponent', () => {
+  let component: WorkingTimeComponent;
+  let fixture: ComponentFixture<WorkingTimeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WorkingTimeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkingTimeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
