0369eb464de1eabc179c459cfa262881a58bfb54
vschefer <vanessa.schefer@hotmail.com>
2018-10-17 16:33:49 +0200

    show and hide todo-detail

--- zyt-app/src/app/todo/todo-detail/todo-detail.component.ts
+++ zyt-app/src/app/todo/todo-detail/todo-detail.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import {SideBarService} from '../todo-detail/todo-detail.service'
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
-  styleUrls: ['./todo-detail.component.css']
+  styleUrls: ['./todo-detail.component.scss']
 })
 export class TodoDetailComponent implements OnInit {
+  @HostBinding('class.is-open')
+  isOpen = false;
 
-  constructor() { }
+  constructor(
+    private sideBarService: SideBarService
+  ) { }
 
   ngOnInit() {
+    this.sideBarService.change.subscribe(isOpen => {
+      this.isOpen = isOpen;
+    });
   }
 
 }
--- zyt-app/src/app/todo/todo-list/todo-list.component.ts
+++ zyt-app/src/app/todo/todo-list/todo-list.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Directive,Component, OnInit, Input, Output,ElementRef, EventEmitter, HostListener } from '@angular/core';
+import {SideBarService} from '../todo-detail/todo-detail.service'
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.scss']
+})
+@Directive({
+  selector: '[toggleDetail]',
 })
 export class TodoListComponent implements OnInit {
+  windowWidth = document.body.clientWidth;
+  spalte = this.windowWidth / 4;
+  constructor(private sideBarService: SideBarService) {}
 
-  constructor() { }
+  tog(){
+    this.sideBarService.toggle();
+    document.querySelector('body').classList.toggle('open')
+    
+    if(document.querySelector('.open')){
+      document.body.style.width = this.spalte * 5 + 'px';
+      document.body.style.width = 'scroll';
+    }else{
+      document.body.style.width = '100vw'
+      document.body.style.overflowX = 'hidden'
+    }
+}
 
   ngOnInit() {
   }
