d90d28681512bd2de9a32a1dcf1ab8d93e70234a
vschefer <vanessa.schefer@hotmail.com>
2018-12-09 21:22:13 +0100

    style all ressources

--- zyt-app/src/app/all-ressource/all-resource.component.ts
+++ zyt-app/src/app/all-ressource/all-resource.component.ts
@@ -15,18 +15,53 @@ export class AllRessourceComponent extends WorkingWeekComponent implements OnIni
     
   }
   
-  showDay(){
-    let button = document.querySelector('.test')
+  showDay(event){
+    console.log(this)
+    let button = event.path[0]
+
+
     let weekDays = document.querySelectorAll('.all__ressource')
-    let growDay = button.parentElement
+    let growDay = event.path[1]
     let myArray = Array.from(weekDays)
-    //  weekDays.classList.add('shrink')
-    growDay.classList.add('grow')
-    
-    
+    for(let i = 0; i < weekDays.length; i++){
+      weekDays[i].classList.remove('grow')
+    }
+    if(growDay.classList.contains('grow')){
+      growDay.classList.remove('grow')
+    }else{
+      growDay.classList.add('grow')
+    }
+
+
+      for(let i = 0; i < weekDays.length; i++){
+        weekDays[i].classList.add('shrink')
+      } 
   }
+
+  toDay(){
+    let date = new Date()
+    let today = date.getDay()
+    let weekDays = document.querySelectorAll('.all__ressource')
+    for(let i = 0; i < weekDays.length; i++){
+      if(today != 0 && today != 6){
+        if((today -1) == i){
+          weekDays[i].classList.add('grow')
+        }else{
+          weekDays[i].classList.add('shrink')
+        }
+      }else if(i == 0){
+        weekDays[i].classList.add('grow')
+      }else{
+        weekDays[i].classList.add('shrink')
+      }
+   
+    }
+  }
+     
   ngOnInit() {
-    this.showDay()
+    // this.showDay()
+    this.toDay()
+    
   }
   
 }
