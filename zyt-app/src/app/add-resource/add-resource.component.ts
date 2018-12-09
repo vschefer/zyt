48a0baa0fa45a9522c456b18109f6ec8326f378e
Michael Fischer <doptrois@gmail.com>
2018-12-09 01:23:55 +0100

    added condition user options based on selected project.

--- zyt-app/src/app/add-resource/add-resource.component.ts
+++ zyt-app/src/app/add-resource/add-resource.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatSnackBar} from '@angular/material';
+import {HttpClient} from '@angular/common/http'
+import { ServerService } from '../server.service';
 @Component({
   selector: 'app-add-resouce',
   templateUrl: './add-resource.component.html',
@@ -7,15 +9,45 @@ import {MatSnackBar} from '@angular/material';
 })
 export class AddResourceComponent implements OnInit {
 
-  constructor(public snackBar: MatSnackBar) { }
+  constructor(public snackBar: MatSnackBar, private serverService: ServerService) { }
 message = "Resource wurde hinzugefügt";
 action = "Ok";
+data:Object;
+users:Array<String>;
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
       duration: 2000,
     });
   }
+  getProjects() {
+    this.serverService.getAll('http://localhost:9000/api/projects').subscribe((response) => {
+      this.data = response.json();
+      },
+      (error) => console.log(error)
+    )
+  }
+  bleh(id) {
+    console.log(id);
+    this.serverService.getAll('http://localhost:9000/api/projects/' + id).subscribe((response) => {
+      let users = [];
+
+      let project = response.json();
+
+      project.assigned_users.forEach((user) => {
+        users.push(user.first_name);
+      });
+
+      this.users = users;
+      console.log(this.users);
+      },
+      (error) => console.log(error)
+    )
+
+  }
+
   ngOnInit() {
+    this.getProjects();
+
   }
 
 }
