13eb5a6d19e4dfc75d46d86c511f814a4990896b
vschefer <vanessa.schefer@hotmail.com>
2018-12-22 02:47:52 +0100

    show me ressources of this week, save ressource on click

--- zyt-app/src/app/add-resource/add-resource.component.ts
+++ zyt-app/src/app/add-resource/add-resource.component.ts
@@ -8,20 +8,21 @@ import { ServerService } from '../server.service';
   styleUrls: ['./add-resource.component.scss']
 })
 export class AddResourceComponent  implements OnInit {
-projects:Object
-specificProject: any
-selectedOption: any;
-selectedUser:any;
-objectKeys = Object.keys;
-ressource: Object
-time
-
+  projects:Object
+  specificProject: any
+  selectedOption: any;
+  selectedUser:any;
+  objectKeys = Object.keys;
+  ressource: Object
+  time
+  project
+  proj
   constructor(public snackBar: MatSnackBar, private serverService: ServerService) { }
-message = "Resource wurde hinzugefügt";
-action = "Ok";
-data:Object;
-users:Array<String>;
-
+  message = "Resource wurde hinzugefügt";
+  action = "Ok";
+  data:Object;
+  users:Array<String>;
+  
   openSnackBar() {
     this.snackBar.open(this.message, this.action, {
       duration: 2000,
@@ -29,56 +30,57 @@ users:Array<String>;
   }
   getProjects() {
     this.serverService.getAll('http://localhost:9000/api/projects').subscribe((response) => {
-      this.data = response.json();
-      },
-      (error) => console.log(error)
-    )
-  }
-  getProject(id) {
-    console.log(id);
-    this.serverService.getAll('http://localhost:9000/api/projects/' + id).subscribe((response) => {
-      let users = [];
+    this.data = response.json();
+  },
+  (error) => console.log(error)
+)
+}
+getProject(id) {
+  console.log(id);
+  this.serverService.getAll('http://localhost:9000/api/projects/' + id).subscribe((response) => {
+  let users = [];
+  
+  let project = response.json();
+  
+  project.assigned_users.forEach((user) => {
+    users.push({
+      "first_name": user.first_name,
+      "surname": user.surname,
+      "_id": user._id
+    });
+  });
+  
+  this.users = users;
+  console.log(this.users);
+  this.project = project
+},
+(error) => console.log(error)
+)
 
-      let project = response.json();
+}
 
-      project.assigned_users.forEach((user) => {
-        users.push({
-          "first_name": user.first_name,
-          "surname": user.surname,
-          "_id": user._id
-        });
-      });
-
-      this.users = users;
-      console.log(this.users);
-      },
-      (error) => console.log(error)
-    )
-
-  }
-
-  onSave(){
-    let nextButton = document.querySelector('.next')
-    let prevButton = document.querySelector('.prev')
-    let wrapper = document.querySelector('.wrapper')
+onSave(){
+  let nextButton = document.querySelector('.next')
+  let prevButton = document.querySelector('.prev')
+  let wrapper = document.querySelector('.wrapper')
+  console.log(this.proj)
+  
+  this.ressource={
+    project: this.proj,
+    user: this.selectedUser,
+    recorded_time: this.time
     
-    
-    this.ressource={
-      project: this.selectedOption,
-      user: this.selectedUser,
-      recorded_time: this.time
-      
-    }
-    
-  //   this.serverService.add(this.ressource, 'http://localhost:9000/api/projects').subscribe(
-  //   (response)=> console.log(response),
-  // )
-    this.openSnackBar()
-  } 
-
-  ngOnInit() {
-    this.getProjects();
-
   }
+  
+  this.serverService.add(this.ressource, 'http://localhost:9000/api/ressources').subscribe(
+  (response)=> console.log(response),
+)
+this.openSnackBar()
+} 
+
+ngOnInit() {
+  this.getProjects();
+  
+}
 
 }
--- zyt-app/src/app/all-ressource/all-resource.component.ts
+++ zyt-app/src/app/all-ressource/all-resource.component.ts
@@ -1,78 +1,133 @@
 import { Component, OnInit } from '@angular/core';
 import {WorkingWeekComponent} from '../working-time/working-week/working-week.component'
 import { ServerService } from '../server.service';
+import * as moment from 'moment';
 @Component({
   selector: 'app-all-ressource',
   templateUrl: './all-resource.component.html',
   styleUrls: ['./all-resource.component.scss']
 })
 export class AllRessourceComponent extends WorkingWeekComponent implements OnInit {
-  ressource: Object
+  ressource
+  now = moment().format('YYYY-MM-DD');
+  mo
+  fr
+  firstDay
+  lastDay
+  startDay
+  weekRes:Array<String>;
+  dates: Object
   constructor(private serverService: ServerService) {
     super()
     this.monday
     this.friday
     this.getWeek()
-
+    console.log(this.now)
     
+  }
+  getWeek(){
+    let curr = new Date;
+    let first = curr.getDate() - curr.getDay();
+    first = first + 1
+    let last = first + 6;
+    let end = first + 4;
+    this.firstDay = first
+    let mon = new Date(curr.setDate(first))
+    let sun = new Date(curr.setDate(last))
+    let fri = new Date(curr.setDate(end))
+    this.mo = mon
+    console.log(this.firstDay)
+    this.lastDay = end
+    this.fr = fri
+    this.startDay = this.mo.toISOString().slice(0, 10)
+    
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    this.monday = mon.toLocaleDateString('de-DE', options)
+    this.sunday =sun.toLocaleDateString('de-DE', options)
+    this.friday =fri.toLocaleDateString('de-DE', options)
   }
   getRessource(){
     this.serverService.getAll('http://localhost:9000/api/ressources').subscribe(
     (response)=> {
+      let weekRes = []
       this.ressource = response.json();
-      console.log(this.ressource)
+      this.ressource.forEach(element => { 
+        
+        if(moment(element.start).isBetween(this.mo.toISOString().slice(0, 10), this.fr.toISOString().slice(0, 10)) 
+        || moment(element.stop).isBetween(this.mo.toISOString().slice(0, 10), this.fr.toISOString().slice(0, 10))){
+          let  startDate = moment(element.start);
+          let    endDate = moment(element.stop);
+          let s = []
+          let now = startDate, dates = [];
+          
+          while (now.isBefore(endDate) || now.isSame(endDate)) {
+            dates.push(now.format('YYYY-MM-DD'));
+            now.add(1, 'days');
+          }
+          
+          dates.forEach((e)=>{
+            s.push(new Date(e).getDate()) 
+          })
+          
+          element.days = s
+          weekRes.push(element)
+          
+        }
+      });
+      this.weekRes = weekRes
+      
     },
     (error) => console.log(error)
   )
 }
-  showDay(event){
-    console.log(this)
-    let button = event.path[0]
 
-
-    let weekDays = document.querySelectorAll('.all__ressource')
-    let growDay = event.path[1]
-    let myArray = Array.from(weekDays)
-    for(let i = 0; i < weekDays.length; i++){
-      weekDays[i].classList.remove('grow')
-    }
-    if(growDay.classList.contains('grow')){
-      growDay.classList.remove('grow')
-    }else{
-      growDay.classList.add('grow')
-    }
-
-
-      for(let i = 0; i < weekDays.length; i++){
-        weekDays[i].classList.add('shrink')
-      } 
+showDay(event){
+  console.log(this)
+  let button = event.path[0]
+  
+  
+  let weekDays = document.querySelectorAll('.all__ressource')
+  let growDay = event.path[1]
+  let myArray = Array.from(weekDays)
+  for(let i = 0; i < weekDays.length; i++){
+    weekDays[i].classList.remove('grow')
   }
+  if(growDay.classList.contains('grow')){
+    growDay.classList.remove('grow')
+  }else{
+    growDay.classList.add('grow')
+  }
+  
+  
+  for(let i = 0; i < weekDays.length; i++){
+    weekDays[i].classList.add('shrink')
+  } 
+}
 
-  toDay(){
-    let date = new Date()
-    let today = date.getDay()
-    let weekDays = document.querySelectorAll('.all__ressource')
-    for(let i = 0; i < weekDays.length; i++){
-      if(today != 0 && today != 6){
-        if((today -1) == i){
-          weekDays[i].classList.add('grow')
-        }else{
-          weekDays[i].classList.add('shrink')
-        }
-      }else if(i == 0){
+toDay(){
+  let date = new Date()
+  let today = date.getDay()
+  let weekDays = document.querySelectorAll('.all__ressource')
+  for(let i = 0; i < weekDays.length; i++){
+    if(today != 0 && today != 6){
+      if((today -1) == i){
         weekDays[i].classList.add('grow')
       }else{
         weekDays[i].classList.add('shrink')
       }
-   
+    }else if(i == 0){
+      weekDays[i].classList.add('grow')
+    }else{
+      weekDays[i].classList.add('shrink')
     }
-  }
-     
-  ngOnInit() {
-    // this.showDay()
-    this.toDay()
-    this.getRessource()
     
   }
-  
+}
+
+ngOnInit() {
+  // this.showDay()
+  this.toDay()
+  this.getRessource() 
+}
+
 }
