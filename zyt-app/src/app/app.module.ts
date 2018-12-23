090634cef836525b30a0cb9965b7f739f9fd46de
Michael Fischer <doptrois@gmail.com>
2018-12-23 12:23:09 +0100

    fixed bug dublicate identifier.

--- zyt-app/src/app/app.module.ts
+++ zyt-app/src/app/app.module.ts
@@ -154,12 +154,12 @@ import { UpdateUserComponent } from './user/update-user/update-user.component';
       },
       {
         path: 'todo',
-        component: TodoListComponent,,
+        component: TodoListComponent,
         canActivate: [AuthGuard]
       },
       {
         path: 'todo/add',
-        component: AddTodoComponent,,
+        component: AddTodoComponent,
         canActivate: [AuthGuard]
       },
       {
