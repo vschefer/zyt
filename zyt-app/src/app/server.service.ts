1e1d005dd74900b4bf0954fd6dfd91e2017ed188
doptrois <doptrois@gmail.com>
2018-12-30 00:11:25 +0100

    added static function Reason: Inserting modules into constructor in WorkingWeekComponent failed due to classes that extend from WorkingWeekComponent.

--- zyt-app/src/app/server.service.ts
+++ zyt-app/src/app/server.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import { map } from "rxjs/operators";
+import { Observable } from 'rxjs';
+import { from } from 'rxjs';
 @Injectable()
 export class ServerService {
     headers: Headers;
@@ -23,6 +25,25 @@ export class ServerService {
         this.headers = new Headers();
         this.auth();
         return  this.http.get(url,{headers: this.headers});
+    }
+    
+    static getAllStatic(url:string){
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('x-auth-token', JSON.parse(localStorage.getItem('currentUser')).token);
+        
+        // Convert fetch to RxJS Observable
+        var response = from(
+          fetch(url, {
+            cache: "no-cache",
+            headers: {
+              'Content-Type': 'application/json',
+              'x-auth-token': JSON.parse(localStorage.getItem('currentUser')).token,
+            }
+          }));
+        // result.subscribe(x => console.log(x), e => console.error(e));
+        
+        return response;
     }
 
     put(url:string, data:object) {
