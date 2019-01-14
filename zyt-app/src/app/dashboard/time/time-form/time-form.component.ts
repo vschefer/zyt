8181f4cfe36703f2b02a5f9fe5d68d85c4c8045a
doptrois <doptrois@gmail.com>
2019-01-15 00:49:47 +0100

    fixed dialog and css styling issues

--- zyt-app/src/app/dashboard/time/time-form/time-form.component.ts
+++ zyt-app/src/app/dashboard/time/time-form/time-form.component.ts
@@ -45,6 +45,7 @@ getThisProject(id) {
   this.serverService.getAll('http://localhost:9000/api/projects/' + id).subscribe((response) => {
   
   this.specificProject = response.json();
+  this.selectedJob = this.specificProject.name;
 },
 (error) => console.log(error)
 )
@@ -54,19 +55,28 @@ toArray(answers: object) {
   return Object.keys(answers).map(key => answers[key])
 }
 
+getPositionName(id) {
+  this.selectedPositionName = this.specificProject.positions.filter((pos) => {
+  	return pos._id === id;
+  })[0].name;
+  debugger;
+}
+
 
 postExpense(){
   console.log(this.selectedPosition);
   const month = this.date.getUTCMonth() + 1; //months from 1-12
   const day = this.date.getUTCDate() + 2;
   const year = this.date.getUTCFullYear();
+  
   this.expense={
     recorded_time: this.time,
     comment: this.activity,
     affected_date: `${year}-${month}-${day}`,
-    project: this.selectedJob,
+    project: this.specificProject._id,
     position: this.selectedPosition
   }
+  debugger;
   console.log(this.expense)
   this.serverService.add(this.expense, 'http://localhost:9000/api/expenses').subscribe(
   (response)=> console.log(response),
