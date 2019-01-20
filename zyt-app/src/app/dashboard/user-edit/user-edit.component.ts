8c81cb2ccdb83a5a9a1b34f6e57ceeba7342f414
vschefer <vanessa.schefer@hotmail.com>
2019-01-20 23:36:33 +0100

    styling fixes for user edit and add change password

--- zyt-app/src/app/dashboard/user-edit/user-edit.component.ts
+++ zyt-app/src/app/dashboard/user-edit/user-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerService } from '../../server.service';
+import {MatSnackBar, MatInputModule, MatSelectModule} from '@angular/material';
+
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
@@ -7,10 +9,14 @@ import { ServerService } from '../../server.service';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor(private serverService: ServerService) {}
+  constructor(private serverService: ServerService, public snackBar: MatSnackBar) {
+  }
 
   avatars:Array<Object>;
-
+  password
+  passwordRepeat
+  message = "Dein Passwort wurde geändert";
+  action = "OK"
   getAvatars() {
     this.serverService.getAll('//localhost:9000/api/avatars').subscribe((response) => {
       this.avatars = response.json();
@@ -19,7 +25,11 @@ export class UserEditComponent implements OnInit {
       (error) => console.log(error)
     )
   }
-
+  openSnackBar() {
+    this.snackBar.open(this.message, this.action, {
+      duration: 5000,
+    });
+  }
   changeAvatar(avatarID) {
     const userID = JSON.parse(localStorage.getItem('currentUser'))._id;
     this.serverService.put('//localhost:9000/api/users/' + userID, {avatar: avatarID}).subscribe((response) => {
@@ -27,6 +37,20 @@ export class UserEditComponent implements OnInit {
       },
       (error) => console.log(error)
     )
+  }
+  changePassword(){
+    const userID = JSON.parse(localStorage.getItem('currentUser'))._id;
+  if(this.password != undefined && this.passwordRepeat != undefined ){
+    if(this.password == this.passwordRepeat) {
+      this.serverService.put('//localhost:9000/api/users/' + userID, {password: this.password}).subscribe((response) => {
+        console.log(response);
+        },
+        (error) => console.log(error)
+      )
+      this.openSnackBar()
+    }
+  }
+ 
   }
 
   ngOnInit() {
