42623ef51d0ae97112ad0b13f6c99161de5b9117
doptrois <doptrois@gmail.com>
2019-02-17 11:08:30 +0100

    fixed compiling error

--- zyt-app/src/app/_helpers/jwt.interceptor.ts
+++ zyt-app/src/app/_helpers/jwt.interceptor.ts
@@ -10,7 +10,7 @@ export class JwtInterceptor implements HttpInterceptor {
     if (currentUser && currentUser.token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
+          Authorization: `Bearer ${currentUser['token']}`
         }
       });
     }
